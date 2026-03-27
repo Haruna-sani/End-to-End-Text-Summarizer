4e01f3c9f89f1e5da16b032d7732fe7f0176f3c2
Haruna-sani <harunasulesani.gmail.com>
2026-03-27 01:48:44 -0700

    folder structure added

---
+++ template.py
@@ -0,0 +1,56 @@
+## Create the template of the project structure in a pythonic way..
+## Import the necessary Python libraries
+
+import os
+from pathlib import Path
+import logging
+
+#############################################################################
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+## Initiating the project structure
+
+project_name = "LEO-SUMMARIZER"
+
+#############################################################################
+
+list_of_files = [
+    ".github/workflow/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/experimentation.ipynb"
+]
+
+#############################################################################
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir!= "":
+        os.makedirs(filedir, exist_ok= True)
+        logging.info(f"Creatin directory: {filedir} for the file {filename}")
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+            
+            
+        
